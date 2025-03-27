73b6702fd5f4065c7fc98681fba24ab78c08f786
YU-1605 <tagu1517@gmail.com>
2025-03-27 20:24:26 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,26 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		
+		// 加藤太郎
+		KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
+		katoTaro.setGivenName();
+		katoTaro.execIntroduce();
+		System.out.println();
+		
+		// 加藤一郎
+		KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+		katoIchiro.setGivenName();
+		katoIchiro.execIntroduce();
+		System.out.println();
+		
+		// 加藤花子
+		KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();
+		katoHanako.setGuvenName();
+		katoHanako.execIntroduce();
+
+	}
+
+}
---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+
+	
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です");
+	}
+	
+	public void setGuvenName() {
+		this.givenName = "花子";
+	}
+
+}
